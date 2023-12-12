b417a789f8cf912965e3b37c8d7fd61466384694
LizFujimori <liizfdaan@gmail.com>
2023-12-11 23:58:30 -0400

    detaque
    parece um gatinho olhando de lado..

---
+++ src/destaque.jsx
@@ -0,0 +1,32 @@
+import "./home.css"
+import Card from "./card";
+import "./home.css"
+
+export default function destaque(){
+    const videoP = JSON.parse(localStorage.getItem('Lista')) || [];
+    console.log(videoP);
+    return(
+        <div>
+            <body>
+                <div class= "container">
+                    <nav class= "sidebar">
+                        <ul>
+                            <li><a href="./">Home</a></li>
+                            <li><a href="/destaque"></a>Destaques</li>
+                            <li><a href="/registro">Adicionar Vídeo</a></li>
+                        </ul>
+                    </nav>
+                    <div>
+                        <div className= "values-area">
+                            {videoP.slice(-4).map((item, index) => (
+                                <div key= {item.id}>
+                                    <Card key= {index} item= {item}></Card>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </div>
+    )
+}
