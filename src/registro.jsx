3890b860fb9c448cc8ea05f5aad1ad79964d29ad
LizFujimori <liizfdaan@gmail.com>
2023-12-12 00:21:09 -0400

    registro
    to com mt medo de perder tudp, ta travando mt scrr

--- src/registro.jsx
+++ src/registro.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function registro() {
-    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"))
-    const [lista, setLista] = useState(listaLocalStorage);
+    const listaLocalStorage = JSON.parse (localStorage.getItem("Lista"))
+    const [lista, setLista] = useState (listaLocalStorage);
+    const [id, setId] = useState (listaLocalStorage [listaLocalStorage.lenght - 1]?.id + 1 || 1);
+    const [Titulo, setTitulo] = useState ("");
+    const [Descricao, setDescricao] = useState ("");
 }
