e9de343ac85246363a4e1c26ece4ae7690b9300b
LizFujimori <liizfdaan@gmail.com>
2023-12-12 01:12:39 -0400

    registro

--- src/registro.jsx
+++ src/registro.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function registro() {
     const listaLocalStorage = JSON.parse (localStorage.getItem("Lista"))
     const [lista, setLista] = useState (listaLocalStorage);
-    const [id, setId] = useState (listaLocalStorage [listaLocalStorage.lenght - 1]?.id + 1 || 1);
+    const [id, setId] = useState (listaLocalStorage [listaLocalStorage.length - 1]?.id + 1 || 1);
     const [Titulo, setTitulo] = useState ("");
     const [Descricao, setDescricao] = useState ("");
     const [Canal, setCanal] = useState ("");
