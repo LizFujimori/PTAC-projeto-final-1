e3d1d6ba6488d082cc3162db969b4672b742bad9
LizFujimori <liizfdaan@gmail.com>
2023-12-11 23:06:54 -0400

    consegui recuperar
    passando o q eu fiz pra ca

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,30 @@
 export default function Home(){
+    const videos = JSON.parse(localStorage.getItem('Lista')) || [];
+    console.log(videos);
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        <div>
+            <body>
+                <div  class="container">
+                    <nav class="sidebar">
+                        <ul>
+                            <li><a href="./">Home</a></li>
+                            <li><a href="/destaque"></a>Destaques</li>
+                            <li><a href="/registro">Adicionar Vídeo</a></li>
+                        </ul>
+                    </nav>
+                    <div>
+                        <div className="values-area">
+                            {
+                                videos.map((item, index) => (
+                                    <div key={item.id}>
+                                        <Card key={index} item={item}></Card>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </div>
      );
 }
