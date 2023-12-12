a97bdeca3e008d050342f5af340e5977148ecd37
LizFujimori <liizfdaan@gmail.com>
2023-12-12 00:11:02 -0400

    registro
    meu opera bugou, ja pensou trava tudo e eu perco? slk amo falar sozinha

--- src/registro.jsx
+++ src/registro.jsx
@@ -1,2 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+export default function registro() {
+    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"))
+}
